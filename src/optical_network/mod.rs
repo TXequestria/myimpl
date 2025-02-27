6384f3807a49d2651f72421dae6e9e4b0f01ab0a
txeq <txeq@tuta.io>
2025-02-27 13:46:43 +0800

    Added optical network  On branch developing  Changes to be committed: 	new file:   src/optical_network/mod.rs

---
+++ src/optical_network/mod.rs
@@ -0,0 +1,8 @@
+// mimics a multi-band optical network using undirected graphs
+// studies the implications of bandwidth fragmentation
+// and non-linear effects in optical networks
+
+#![allow(unused_imports)]
+
+use crate::dsa::graph::{UnDirectedGraph,DirectedGraph};
+use crate::linear_algebra::matrix::{Matrix,MatrixError};
