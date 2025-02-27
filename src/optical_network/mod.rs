44953bb09d2aac7894d627fbb0524bcd5006a09f
txeq <txeq@tuta.io>
2025-02-27 13:53:50 +0800

    added comments regarding optical_network's rules  On branch developing  Changes to be committed: 	modified:   src/optical_network/mod.rs

--- src/optical_network/mod.rs
+++ src/optical_network/mod.rs
@@ -6,3 +6,15 @@
 
 use crate::dsa::graph::{UnDirectedGraph,DirectedGraph};
 use crate::linear_algebra::matrix::{Matrix,MatrixError};
+
+/* assumes a bunch of nodes in row major order
+suppose a total of M nodes, each nodes have identical bandwidth range 0-N
+then each row is 1 by N representing a node
+total M rows representing all nodes
+Matrix M by N */
+
+/* rules, suppose a task takes bandwith 5 block, and goes through link A->B->C->D
+then A[5] B[5] C[5] D[5] must be all occupied
+same bandwith block number must be used in all accessed link nodes for the same task
+is is called bandwith consistency constraint
+so for the same task it takes up a whole column */
